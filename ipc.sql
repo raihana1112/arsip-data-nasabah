83e51526f5ca714eb70940ef8a5d4fb78b4bf925
raihana1112 <Raihana111201@gmail.com>
2023-10-10 21:26:02 +0700

    database

---
+++ ipc.sql
@@ -0,0 +1,72 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 10, 2023 at 04:25 PM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.1.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ipc`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `username` varchar(15) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `pass` varchar(20) NOT NULL,
+  `confirm_pass` varchar(20) NOT NULL,
+  `nama_tim` varchar(50) NOT NULL,
+  `nama_anggota1` varchar(50) NOT NULL,
+  `nama_anggota2` varchar(50) NOT NULL,
+  `nama_anggota3` varchar(50) NOT NULL,
+  `prodi` varchar(50) NOT NULL,
+  `fakultas` varchar(50) NOT NULL,
+  `univ` varchar(50) NOT NULL,
+  `no_wa` varchar(20) NOT NULL,
+  `file` text NOT NULL,
+  `nama_juri` varchar(30) NOT NULL,
+  `hak_akses` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
